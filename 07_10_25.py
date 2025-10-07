cdcb249d59eaa6704dd5d6e6830e241dcdc66382
ealg935 <ealejandrolg@gmail.com>
2025-10-07 17:14:02 -0600

    Create 07_10_25.py

---
+++ 07_10_25.py
@@ -0,0 +1,16 @@
+nombre = "Edgar"
+palabra = "RECONOCER"
+otra_palabra = "PALABRA"
+"""print(len(nombre))
+
+print(nombre[1:])
+
+
+print(len(palabra[5:]))
+
+print(palabra[::-1])
+
+print(otra_palabra[::-1])"""
+
+word = "nigger" 
+print(word==word[::-1])
