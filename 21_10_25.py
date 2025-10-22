1acee52acce042b5b3b39d1e8c3dda3e69bf4e41
ealg935 <ealejandrolg@gmail.com>
2025-10-22 16:41:06 -0600

    Update 21_10_25.py

---
+++ 21_10_25.py
@@ -0,0 +1,10 @@
+#trabajando con listas #
+
+'''datos = [1,2,"tres",["lunes","martes","miercoles"]]
+print((datos[2])[1])
+print((datos[3])[2][3])'''
+
+numeros = ['uno','dos','tres']
+print(numeros)
+numeros = numeros + ['cuatro','cinco']
+print(len(numeros))
