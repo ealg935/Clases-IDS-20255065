eae8c34ccca3e156b93434dff0b0edc670425fa9
ealg935 <ealejandrolg@gmail.com>
2025-10-28 16:39:52 -0600

    v

--- 22_10_25.py
+++ 22_10_25.py
@@ -52,26 +52,34 @@ else:
 '''
 #23/10/25#
 
-monto = int(input('Monto a pagar: '))
+'''monto = int(input('Monto a pagar: '))
 tipo = input('Tipo (local/internacional): ')
 impuesto = 0
 
 if tipo.lower() == 'local':
     if monto >= 100:
         impuesto = 0.07
-    elif monto >= 75:
-        impuesto = 0.05
     else:
-        impuesto = 0
-print(f'El porcentaje es del {impuesto*100}%')
-
+        if monto >= 75:
+            impuesto = 0.05
+        else:
+            impuesto = 0
 elif tipo.lower() == 'internacional':
     if monto >= 100:
         impuesto = 0.12
     elif monto >= 75:
-        impuesto = 0.09
+        impuesto = 0.10
     else:
         impuesto = 0
-print(f'El porcentaje es del {impuesto*100}%')
 
+else:
+    print('Ese tipo no existe')
+print(f'El tipo {tipo} con monto {monto:,.2f}')
+print(f'paga un impuesto de {monto*impuesto:.2f}%')'''
+
+#Iteraciones#
+nombre = ['Ana', 'Jose', 'Luis']
+
+for x in nombre:
+    print(f'Hola {x}')
 
--- guia_ejercicios.py
+++ guia_ejercicios.py
@@ -7,16 +7,30 @@ else:
     
 #ejercicio 2#
 
-numero = int(input())
+'''numero = int(input())
 if (numero + 1) % 2 == 0:
-    par_post = numero + 1
-else: 
-    par_post = numero + 2
-
-if len(str(numero)[-1]) == float:
-    impar_ant = numero - 1
+    print(numero + 1)
 else:
-    impar_ant = numero - 2
+    print(numero + 2)
 
-print(par_post, impar_ant)
+numero_impar = numero-1
+if (numero_impar) % 2 != 0:
+    print(numero_impar)
+else:
+    print(numero_impar - 1)
+'''
 
+#ejercicio 3#
+
+numero1 = float(input())
+numero2 = float(input())
+numero3 = float(input())
+numero4 = float(input())
+numero5 = float(input())
+numero6 = float(input())
+
+promedio = (numero1 + numero2 + numero3 + numero4 + numero5 + numero6) / 6
+if promedio >= 9.5:
+    print('Gana Premio :)')
+else:
+    print('No Gana Premio :(')
